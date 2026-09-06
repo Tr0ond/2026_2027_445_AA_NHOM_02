ea319399d076738c8ed97bd6984319905cbd3efa
Vinh <xtung44@gmail.com>
2026-09-07 01:05:30 +0700

    Thêm hồ sơ, thông báo và số liệu thống kê dành cho quản trị viên
    Thêm ba API controller và đăng ký các route:
    
    - BE/app/Http/Controllers/Api/HoSoController.php: các endpoint cho hồ sơ người dùng (xem, cập nhật) và đổi mật khẩu có kèm kiểm tra dữ liệu đầu vào (US03).
    - BE/app/Http/Controllers/Api/ThongBaoController.php: danh sách thông báo, đánh dấu đã đọc (cho từng thông báo hoặc tất cả); kiểm tra quyền sở hữu trước khi thực hiện đánh dấu.
    - BE/app/Http/Controllers/Api/Admin/ThongKeController.php: các endpoint báo cáo dành cho quản trị viên (tổng quan, tỷ lệ điểm danh theo lớp, điểm trung bình theo lớp) (US24).
    
    Cập nhật BE/routes/api.php để import các controller mới và thiết lập các route công khai cũng như route dành cho quản trị viên (/ho-so, /doi-mat-khau, /thong-bao và /admin/thong-ke/*).

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\BaoCaoController;
+use App\Http\Controllers\Api\Admin\DanhMucController;
+use App\Http\Controllers\Api\Admin\PhanCongController;
+use App\Http\Controllers\Api\Admin\SinhVienLopController;
+use App\Http\Controllers\Api\Admin\TaiKhoanController;
 use App\Http\Controllers\Api\Admin\ThanhPhanController;
+use App\Http\Controllers\Api\Admin\ThongKeController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DiemDanhSinhVienController;
 use App\Http\Controllers\Api\DiemHocTapController;
 use App\Http\Controllers\Api\DonXinPhepController;
+use App\Http\Controllers\Api\HoSoController;
 use App\Http\Controllers\Api\LichHocController;
 use App\Http\Controllers\Api\LopDayController;
 use App\Http\Controllers\Api\LopHocController;
@@ -12,6 +19,7 @@ use App\Http\Controllers\Api\PhienDiemDanhController;
 use App\Http\Controllers\Api\PhongHocController;
 use App\Http\Controllers\Api\QuanLyDiemDanhController;
 use App\Http\Controllers\Api\TinNhanPhongController;
+use App\Http\Controllers\Api\ThongBaoController;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +37,16 @@ Route::post('/dang-nhap', [AuthController::class, 'dangNhap']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/dang-xuat', [AuthController::class, 'dangXuat']);
     Route::get('/me', [AuthController::class, 'me']);
+
+    // Vinh: hồ sơ cá nhân và đổi mật khẩu.
+    Route::get('/ho-so', [HoSoController::class, 'show']);
+    Route::put('/ho-so', [HoSoController::class, 'update']);
+    Route::put('/doi-mat-khau', [HoSoController::class, 'doiMatKhau']);
+
+    // Vinh: danh sách thông báo và trạng thái đã đọc.
+    Route::get('/thong-bao', [ThongBaoController::class, 'index']);
+    Route::post('/thong-bao/doc-tat-ca', [ThongBaoController::class, 'danhDauTatCaDaDoc']);
+    Route::post('/thong-bao/{thongBao}/da-doc', [ThongBaoController::class, 'danhDauDaDoc']);
 
     // Lịch học của người dùng đang đăng nhập.
     Route::get('/lich-hoc', [LichHocController::class, 'index']);
@@ -98,11 +116,53 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/lop-hoc/{lopHoc}/thanh-phan', [DiemHocTapController::class, 'thanhPhanCuaLop']);
     });
 
-    // Admin: cấu hình điểm thành phần theo lớp.
+    // Vinh: quản trị tài khoản, danh mục, phân công, sinh viên, báo cáo và thống kê.
     Route::middleware('vai_tro:admin')->prefix('admin')->group(function () {
+        Route::get('/tai-khoan', [TaiKhoanController::class, 'index']);
+        Route::post('/tai-khoan', [TaiKhoanController::class, 'store']);
+        Route::put('/tai-khoan/{taiKhoan}', [TaiKhoanController::class, 'update']);
+        Route::delete('/tai-khoan/{taiKhoan}', [TaiKhoanController::class, 'destroy']);
+        Route::post('/tai-khoan/{taiKhoan}/doi-trang-thai', [TaiKhoanController::class, 'doiTrangThai']);
+
+        Route::get('/mon-hoc', [DanhMucController::class, 'danhSachMonHoc']);
+        Route::post('/mon-hoc', [DanhMucController::class, 'luuMonHoc']);
+        Route::post('/mon-hoc/{monHoc}', [DanhMucController::class, 'luuMonHoc']);
+        Route::delete('/mon-hoc/{monHoc}', [DanhMucController::class, 'xoaMonHoc']);
+
+        Route::get('/lop-hoc', [DanhMucController::class, 'danhSachLopHoc']);
+        Route::post('/lop-hoc', [DanhMucController::class, 'luuLopHoc']);
+        Route::post('/lop-hoc/{lopHoc}', [DanhMucController::class, 'luuLopHoc']);
+        Route::delete('/lop-hoc/{lopHoc}', [DanhMucController::class, 'xoaLopHoc']);
+        Route::get('/lop-hoc/{lopHoc}/lich-hoc', [DanhMucController::class, 'lichHocCuaLop']);
+        Route::post('/lop-hoc/{lopHoc}/lich-hoc', [DanhMucController::class, 'luuLichHoc']);
+        Route::post('/lop-hoc/{lopHoc}/lich-hoc-nhanh', [DanhMucController::class, 'taoLichNhanh']);
+        Route::delete('/lich-hoc/{lichHoc}', [DanhMucController::class, 'xoaLichHoc']);
+
         Route::get('/lop-hoc/{lopHoc}/thanh-phan', [ThanhPhanController::class, 'index']);
         Route::post('/lop-hoc/{lopHoc}/thanh-phan', [ThanhPhanController::class, 'store']);
         Route::put('/thanh-phan/{thanhPhan}', [ThanhPhanController::class, 'update']);
         Route::delete('/thanh-phan/{thanhPhan}', [ThanhPhanController::class, 'destroy']);
+
+        Route::get('/phan-cong', [PhanCongController::class, 'index']);
+        Route::get('/phan-cong/giang-vien', [PhanCongController::class, 'danhSachGiangVien']);
+        Route::post('/phan-cong', [PhanCongController::class, 'store']);
+        Route::delete('/phan-cong', [PhanCongController::class, 'destroy']);
+        Route::delete('/phan-cong/{phanCong}', [PhanCongController::class, 'destroy']);
+
+        Route::get('/sinh-vien', [SinhVienLopController::class, 'index']);
+        Route::get('/lop-hoc/{lopHoc}/sinh-vien', [SinhVienLopController::class, 'theoLop']);
+        Route::get('/lop-hoc/{lopHoc}/sinh-vien-ngoai', [SinhVienLopController::class, 'chuaThuocLop']);
+        Route::post('/lop-hoc/{lopHoc}/sinh-vien', [SinhVienLopController::class, 'themVaoLop']);
+        Route::delete('/lop-hoc/{lopHoc}/sinh-vien/{sinhVien}', [SinhVienLopController::class, 'xoaKhoiLop']);
+
+        Route::get('/bao-cao/diem-danh/{lopHoc}', [BaoCaoController::class, 'tongHopDiemDanh']);
+        Route::get('/bao-cao/diem-danh/{lopHoc}/xuat', [BaoCaoController::class, 'xuatDiemDanh'])
+            ->middleware('token.query');
+        Route::get('/bao-cao/diem/{lopHoc}/xuat', [BaoCaoController::class, 'xuatDiem'])
+            ->middleware('token.query');
+
+        Route::get('/thong-ke/tong-quan', [ThongKeController::class, 'tongQuan']);
+        Route::get('/thong-ke/chuyen-can-theo-lop', [ThongKeController::class, 'chuyenCanTheoLop']);
+        Route::get('/thong-ke/diem-theo-lop', [ThongKeController::class, 'diemTheoLop']);
     });
 });
